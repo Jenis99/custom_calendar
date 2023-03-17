8a6921945aeb8820e22b83d1477045346aa9b804
krutikagabani <jeniskapadiya99@gmail.com>
2023-03-17 11:57:58 +0530

    .

--- lib/screens/FirstScreen.dart
+++ lib/screens/FirstScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:calendar_timeline/calendar_timeline.dart';
+
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
 
@@ -14,100 +15,113 @@ class _FirstScreenState extends State<FirstScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child:SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 10.0,),
-              Row(
-                children: [
-                   Container(
-                      margin: EdgeInsets.only(left: 15.0),
-                      padding: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        border:Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(50.0),
-                        ),
-                      child: Image.asset("img/leftarrow.png",width: 12.0,)),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: Text("Date Confirm",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25.0
-                      ),),
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              children: [
+                Container(
+                    margin: EdgeInsets.only(left: 15.0),
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(50.0),
                     ),
-                  )
-                ],
-              ),
-              SizedBox(height: 20.0,),
-              CalendarTimeline(
-                initialDate: DateTime.now(),
-                firstDate: DateTime(2019, 1, 15),
-                lastDate: DateTime(2030, 11, 20),
-                onDateSelected: (date) => print(date),
-                leftMargin: 20,
-                monthColor: Colors.red[400],
-                dayColor: Colors.grey,
-                // dayNameColor: Colors.grey,
-                activeDayColor: Colors.white,                
-                activeBackgroundDayColor: Colors.red[400],  
-                dotsColor: Colors.transparent,
-                locale: 'en_ISO',
-                
-                
-              ),
-            SizedBox(height: 20.0,),
-            Image.asset("img/Home.png",width: 400.0,),
-            SizedBox(height: 20.0,),
-            Text("Skipped",style: TextStyle(
-              color: Colors.red[400],
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0
-            ),),
-            SizedBox(height: 20.0,),
+                    child: Image.asset(
+                      "img/leftarrow.png",
+                      width: 12.0,
+                    )),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Date Confirm",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 25.0),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            CalendarTimeline(
+              initialDate: DateTime.now(),
+              firstDate: DateTime(2019, 1, 15),
+              lastDate: DateTime(2030, 11, 20),
+              onDateSelected: (date) => print(date),
+              leftMargin: 20,
+              monthColor: Colors.red[400],
+              dayColor: Colors.grey,
+              // dayNameColor: Colors.grey,
+              activeDayColor: Colors.white,
+              activeBackgroundDayColor: Colors.red[400],
+              dotsColor: Colors.transparent,
+              locale: 'en_ISO',
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Image.asset(
+              "img/Home.png",
+              width: 400.0,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Skipped",
+              style: TextStyle(
+                  color: Colors.red[400],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
             GestureDetector(
-              onTap: (){},
+              onTap: () {},
               child: Container(
                 margin: EdgeInsets.all(15.0),
-                width: MediaQuery.of(context).size.width, 
+                width: MediaQuery.of(context).size.width,
                 height: 60.0,
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
-                  color:Colors.grey[850],
-                  borderRadius: BorderRadius.circular(15.0)
+                    color: Colors.grey[850],
+                    borderRadius: BorderRadius.circular(15.0)),
+                child: Text(
+                  "Unskip",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
                 ),
-                child:Text("Unskip",style:TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white
-                ),) ,
               ),
             ),
             GestureDetector(
-              onTap: (){},
+              onTap: () {},
               child: Container(
-                 margin: EdgeInsets.all(15.0),
-                width: MediaQuery.of(context).size.width, 
+                margin: EdgeInsets.all(15.0),
+                width: MediaQuery.of(context).size.width,
                 height: 60.0,
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.red.shade400),
                   borderRadius: BorderRadius.circular(15.0),
                 ),
-                child:Text("Pause Indefinitely",style:TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.red[400],
-                ),) ,
+                child: Text(
+                  "Pause Indefinitely",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.red[400],
+                  ),
+                ),
               ),
             ),
-          //  ElevatedButton(onPressed: (){
-
-          //  },style: ElevatedButton.styleFrom(
-          //   shape: RoundedRectangleBorder(
-          //                   borderRadius: BorderRadius.circular(10.0)),
-          //  ), child: Text("Unskip"))
-            ],
-          ),
-        )),
+          ],
+        ),
+      )),
     );
   }
 }
--- lib/screens/SecondScreen.dart
+++ lib/screens/SecondScreen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:table_calendar/table_calendar.dart';
+
 class SecondScreen extends StatefulWidget {
   const SecondScreen({super.key});
 
@@ -12,11 +13,9 @@ class SecondScreen extends StatefulWidget {
 }
 
 class _SecondScreenState extends State<SecondScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.week;
 
-
-  CalendarFormat _calendarFormat=CalendarFormat.week;
-
-  DateTime _focusedDay=DateTime.now();
+  DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
   @override
@@ -24,226 +23,245 @@ class _SecondScreenState extends State<SecondScreen> {
     // TODO: implement initState
     super.initState();
     setState(() {
-      _selectedDay=_focusedDay;
+      _selectedDay = _focusedDay;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                     Container(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
                       margin: EdgeInsets.only(left: 15.0),
                       padding: EdgeInsets.all(10.0),
                       decoration: BoxDecoration(
-                        border:Border.all(color: Colors.grey),
+                        border: Border.all(color: Colors.grey),
                         borderRadius: BorderRadius.circular(50.0),
-                        ),
-                      child: Image.asset("img/leftarrow.png",width: 12.0,)),
-                    Container(
-                      alignment: Alignment.center,
-                      child: Text("Schedual Date",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25.0
-                      ),),
+                      ),
+                      child: Image.asset(
+                        "img/leftarrow.png",
+                        width: 12.0,
+                      )),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Schedual Date",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 25.0),
                     ),
-                    IconButton(onPressed: (){},
-                     icon: Icon(Icons.settings))
-                  ],
-                ),
-                SizedBox(height: 20.0,),
-                Text("Step-1",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                      ),),
-                SizedBox(height: 20.0,),
-                Text("Your Match",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                      ),  ),
-                SizedBox(height: 20.0,),
-                Container(
-                  height: 120.0,
-                  decoration: BoxDecoration(
-                     shape: BoxShape.circle,
-                      image: DecorationImage(
-                      image: AssetImage("img/profile.jpg",),
-                      // fit: BoxFit.fill
-                    ),),
                   ),
-                SizedBox(height: 20.0,),
-                Text("I'm available on the",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                      ),),
-                Container(
-                  margin: EdgeInsets.only(left: 5.0,right: 5.0),
-                  child: TableCalendar(
-                    
+                  IconButton(onPressed: () {}, icon: Icon(Icons.settings))
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Step-1",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Your Match",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 120.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(
+                      "img/profile.jpg",
+                    ),
+                    // fit: BoxFit.fill
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "I'm available on the",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 5.0, right: 5.0),
+                child: TableCalendar(
                   firstDay: DateTime.utc(2010, 10, 16),
                   lastDay: DateTime.utc(2030, 3, 14),
                   focusedDay: DateTime.now(),
-                  
-                      
                   onDaySelected: ((selectedDay, focusedDay) {
                     if (!isSameDay(_selectedDay, selectedDay)) {
                       setState(() {
                         _focusedDay = focusedDay;
                         _selectedDay = selectedDay;
-                        
                       });
                     }
-                  }
-                  ),
+                  }),
                   calendarStyle: CalendarStyle(
                     todayDecoration: BoxDecoration(
-                  color: Colors.red[400],
-                  shape: BoxShape.circle,
-                              ),
-                              // highlighted color for selected day
-                              selectedDecoration: BoxDecoration(
-                  color: Colors.red[400],
-                  shape: BoxShape.circle,
-                              ),
+                      color: Colors.red[400],
+                      shape: BoxShape.circle,
+                    ),
+                    // highlighted color for selected day
+                    selectedDecoration: BoxDecoration(
+                      color: Colors.red[400],
+                      shape: BoxShape.circle,
+                    ),
                   ),
                   calendarFormat: _calendarFormat,
                   onFormatChanged: (format) {
                     setState(() {
                       _calendarFormat = format;
                     });
-                    },
-                  
-                  selectedDayPredicate: (day){
+                  },
+                  selectedDayPredicate: (day) {
                     return isSameDay(_selectedDay, day);
                   },
-                  ),
                 ),
-                SizedBox(height: 20.0,),
-                Text("I would like to ",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                      ),),
-                SizedBox(height: 20.0,),
-                Container(
-                  margin: EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        width: 100.0,
-                        height: 50.0,
-                        decoration: BoxDecoration(
-                          color: Colors.red.shade400,
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Center(
-                          child: Text("Bar Drinks",style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15.0
-                          ),),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "I would like to ",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 100.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.red.shade400,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Bar Drinks",
+                          style: TextStyle(color: Colors.white, fontSize: 15.0),
                         ),
                       ),
-                      Container(
-                         width: 100.0,
-                        height: 50.0,
-                        decoration: BoxDecoration(
-                          color: Colors.red.shade400,
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Center(
-                          child: Text("Pub Drinks",style: TextStyle(
-                            color: Colors.white
-                          ),),
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.red.shade400,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Pub Drinks",
+                          style: TextStyle(color: Colors.white),
                         ),
                       ),
-                      Container(
-                        width: 100.0,
-                        height: 50.0,
-                        decoration: BoxDecoration(
-                          color: Colors.red.shade400,
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Center(
-                          child: Text("Walk",style: TextStyle(
-                            color: Colors.white
-                          ),),
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.red.shade400,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Walk",
+                          style: TextStyle(color: Colors.white),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Container(
-                  margin: EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        width: 100.0,
-                        height: 50.0,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Center(
-                          child: Text("Coffee",style: TextStyle(
-                            color: Colors.grey
-                          ),),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 100.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Coffee",
+                          style: TextStyle(color: Colors.grey),
                         ),
                       ),
-                      Container(
-                         width: 100.0,
-                        height: 50.0,
-                        decoration: BoxDecoration(
-                          color: Colors.red.shade400,
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Center(
-                          child: Text("Fun Activity",style: TextStyle(
-                            color: Colors.white
-                          ),),
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.red.shade400,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Fun Activity",
+                          style: TextStyle(color: Colors.white),
                         ),
                       ),
-                      Container(
-                         width: 100.0,
-                        height: 50.0,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Center(
-                          child: Text("Dinner",style: TextStyle(
-                            color: Colors.grey
-                          ),),
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Dinner",
+                          style: TextStyle(color: Colors.grey),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 20.0,),
-                 GestureDetector(
-                onTap: (){},
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              GestureDetector(
+                onTap: () {},
                 child: Container(
                   margin: EdgeInsets.all(15.0),
-                  width: MediaQuery.of(context).size.width, 
+                  width: MediaQuery.of(context).size.width,
                   height: 60.0,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color:Colors.red[400],
-                    borderRadius: BorderRadius.circular(15.0)
+                      color: Colors.red[400],
+                      borderRadius: BorderRadius.circular(15.0)),
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
                   ),
-                  child:Text("Submit",style:TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white
-                  ),) ,
                 ),
               ),
-          ],
-        ),
+            ],
+          ),
         ),
       ),
     );
